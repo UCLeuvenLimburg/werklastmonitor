87a0b968501d407e817ed5efd3db2e7e407f1754
mario aquino <mario.aquino@student.ucll.be>
2019-02-11 19:03:17 +0100

    refactored worksessionRoutes

--- backend/src/routes/worksessionRoutes.js
+++ backend/src/routes/worksessionRoutes.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const Worksession = require('../models/worksession');
+const Workday = require('../models/workday');
 
 const worksessionRouter = express.Router();
 
 worksessionRouter.route('/')
 	.get((req, res) => {
 		Worksession.find({}, (err, worksessions) => {
-			res.json(worksessions);
+			if (err) {
+				res.status('500').send(err);
+			}
+			else {
+				res.json(worksessions);
+			}
 		})
 	})
 
-	.post((req, res) => {
+	.post(async (req, res) => {
 		let worksession = new Worksession();
 		worksession.startDate = req.body.startDate;
 		worksession.endDate = req.body.endDate;
 		worksession.lab = req.body.lab;
-		worksession.workdays = req.body.workdays;
+		// worksession.workdays = req.body.workdays;
+
+		let workdays = req.body.workdays;
+		if(workdays.length > 0) {
+			if(workdays[0].constructor === String){
+				worksession.workdays = await Workday.find({ _id: { $in: workdays } });
+			} else {
+				worksession.workdays = await Workday.insertMany(req.body.workdays);
+			}
+		}
 		console.log(req.body);
 		worksession.save();
 		res.status('201').send(worksession);
@@ -35,11 +50,20 @@ worksessionRouter.route('/:worksession_Id')
 	.get((req, res) => {
 		res.json(req.worksession);
 	})
-	.put((req, res) => {
+	.put(async (req, res) => {
 		req.worksession.startDate = req.body.startDate;
 		req.worksession.endDate = req.body.endDate;
 		req.worksession.lab = req.body.lab;
-		req.worksession.workdays = req.body.workdays;
+		// req.worksession.workdays = req.body.workdays;
+
+		let workdays = req.body.workdays;
+		if(workdays.length > 0) {
+			if(workdays[0].constructor === String) {
+				req.worksession.workdays = await Workday.find( { _id: { $in: workdays } } );
+			} else {
+				req.worksession.workdays = await Workday.insertMany(req.body.workdays);
+			}
+		}
 		req.worksession.save();
 		res.json(req.worksession);
 	})
