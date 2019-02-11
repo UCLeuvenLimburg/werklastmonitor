f27fc9d35d89ff995207c59dc3ef3f9d4660cc3a
mario aquino <mario.aquino@student.ucll.be>
2019-02-11 18:42:50 +0100

    another merge

--- backend/src/models/labModel.js
+++ backend/src/models/labModel.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-<<<<<<< HEAD
-const labModel = new Schema({
-	name: { type: String },
-=======
 
 module.exports = mongoose.model('Lab', new Schema({
 	name: { type: String},
->>>>>>> 5c87538dd676c307d16b596fc2aa1daeab6612f1
 	startDate: { type: Date },
 	endDate: { type: Date },
 	hourEstimate: { type: Number },
