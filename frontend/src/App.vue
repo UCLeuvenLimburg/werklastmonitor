fa95957780e6da5c7d4ed826fdd19852bab8477a
Jordy Rymenants <jordy.rymenants@outlook.com>
2019-02-12 14:04:27 +0100

    Fonts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,4 +4,28 @@
 </template>
 
 <style lang="scss">
+@import 'assets/css/definitions';
+
+@font-face {
+	font-family: 'Aleo';
+	src: url(assets/fonts/Aleo-Regular.ttf);
+	font-weight: normal;
+	font-style: normal;
+}
+@font-face {
+	font-family: 'Aleo';
+	src: url(assets/fonts/Aleo-Bold.ttf);
+	font-weight: bold;
+	font-style: normal;
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	font: 1rem $font;
+}
 </style>
