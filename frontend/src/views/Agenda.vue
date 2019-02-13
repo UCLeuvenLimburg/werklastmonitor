8595cb5775e93e34e1b5e3ff5bbdae48640a69a8
Jordy Rymenants <jordy.rymenants@outlook.com>
2019-02-13 14:42:33 +0100

    Converted Agenda template to pug (and fixed things up a bit)

--- frontend/src/views/Agenda.vue
+++ frontend/src/views/Agenda.vue
@@ -1,36 +1,41 @@
-<template>
-	<div id="app">
-		<h1>Agenda</h1>
-		<calendar-view
-			:show-date="showDate"
-			:events="events"
-			class="theme-default">
-			<calendar-view-header
-				slot="header"
-				slot-scope="t"
-				:header-props="t.headerProps"
-				@input="setShowDate" />
-		</calendar-view>
-	</div>
+<template lang="pug">
+	.page.agenda
+		h1 Agenda
+		calendar-view.calendar.theme-default(
+			:show-date="showDate",
+			:events="events")
+			calendar-view-header(
+				slot="header",
+				slot-scope="t",
+				:header-props="t.headerProps",
+				@input="setShowDate")
 </template>
+
 <script>
 import { CalendarView, CalendarViewHeader } from 'vue-simple-calendar';
 // The next two lines are processed by webpack. If you're using the component without webpack compilation,
 // you should just create <link> elements for these. Both are optional, you can create your own theme if you prefer.
-require('../assets/css/default.css');
+require('vue-simple-calendar/static/css/default.css');
 // require('vue-simple-calendar/static/css/holidays-us.css');
 
 export default {
 	name: 'app',
-	data () {
-		return {
-			showDate: new Date(),
-			events: [{ id: '1', startDate: '2019-02-01', endDate: '2019-02-02', title: 'Werkstukje' }]
-		};
-	},
 	components: {
 		CalendarView,
 		CalendarViewHeader
+	},
+	data () {
+		return {
+			showDate: new Date(),
+			events: [
+				{
+					id: '1',
+					startDate: '2019-02-01',
+					endDate: '2019-02-02',
+					title: 'Werkstukje'
+				}
+			]
+		};
 	},
 	methods: {
 		setShowDate (d) {
@@ -39,10 +44,11 @@ export default {
 	}
 };
 </script>
-<style>
-	#app {
-		height: 67vh;
-		margin-left: auto;
-		margin-right: auto;
-	}
+
+<style lang="scss" scoped>
+.calendar {
+	height: 620px;
+	margin-left: auto;
+	margin-right: auto;
+}
 </style>
