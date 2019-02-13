3b47d93db34b7fc13814cd8104a25a83fcfed59a
Tom <tom.lemmens@student.ucll.be>
2019-02-13 14:51:20 +0100

    CSS voor agenda terug

--- frontend/src/views/Agenda.vue
+++ frontend/src/views/Agenda.vue
@@ -15,7 +15,7 @@
 import { CalendarView, CalendarViewHeader } from 'vue-simple-calendar';
 // The next two lines are processed by webpack. If you're using the component without webpack compilation,
 // you should just create <link> elements for these. Both are optional, you can create your own theme if you prefer.
-require('vue-simple-calendar/static/css/default.css');
+require('../assets/css/default.css');
 // require('vue-simple-calendar/static/css/holidays-us.css');
 
 export default {
