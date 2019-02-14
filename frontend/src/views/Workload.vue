707ffef07c35c99c5eb004d5830b7a86b2b51e6e
Tom L <lemmens.tom11@gmail.com>
2019-02-14 11:12:10 +0100

    Workload wisselen tussen tijdseenheden

--- frontend/src/views/Workload.vue
+++ frontend/src/views/Workload.vue
@@ -1,7 +1,12 @@
 <template lang="pug">
 	.page.workload
 		h1 Werklast
-		p Overzicht van werklast per maand.
+		p
+			| Overzicht van werklast per&nbsp;
+			select(@change='handleChange')
+				option(value='month', selected='selected') Maand
+				option(value='week') Week
+				option(value='day') Dag
 		graph-stackbar(
 			:height="400",
 			:labels="labels",
@@ -18,6 +23,7 @@ export default {
 	name: 'workload',
 	data () {
 		return {
+			// View per month is default
 			names: [ 'Computersystemen', 'Web 1', 'Netwerken', 'BOP' ],
 			values: [
 				[ 10, 5, 5, 5, 10, 11 ],
@@ -27,6 +33,39 @@ export default {
 			],
 			labels: [ '01/19', '02/19', '03/19', '04/19', '05/19', '06/19' ]
 		};
+	},
+	methods: {
+		handleChange (e) {
+			let selection = e.target.options[e.target.options.selectedIndex].valueOf().value;
+			if (selection === 'day') {
+				this.names = [ 'Computersystemen', 'Web 1', 'Netwerken', 'BOP' ];
+				this.values = [
+					[ 3, 5, 2, 5, 2, 3 ],
+					[ 8, 5, 6, 10, 2, 1 ],
+					[ 10, 5, 7, 5, 1, 2 ],
+					[ 8, 10, 7, 4, 2, 2 ]
+				];
+				this.labels = [ 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag' ];
+			} else if (selection === 'week') {
+				this.names = [ 'Computersystemen', 'Web 1', 'Netwerken', 'BOP' ];
+				this.values = [
+					[ 5, 5, 2, 5 ],
+					[ 8, 5, 6, 10 ],
+					[ 8, 5, 7, 5 ],
+					[ 8, 10, 7, 4 ]
+				];
+				this.labels = [ '01-07/02', '08-14/02', '15-23/02', '23-30/02' ];
+			} else {
+				this.names = [ 'Computersystemen', 'Web 1', 'Netwerken', 'BOP' ];
+				this.values = [
+					[ 10, 5, 5, 5, 10, 11 ],
+					[ 8, 5, 6, 10, 2, 5 ],
+					[ 10, 5, 7, 5, 10, 10 ],
+					[ 8, 10, 7, 10, 10, 11 ]
+				];
+				this.labels = [ '01/19', '02/19', '03/19', '04/19', '05/19', '06/19' ];
+			};
+		}
 	}
 };
 </script>
