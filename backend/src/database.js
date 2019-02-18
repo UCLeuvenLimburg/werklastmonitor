7816ceee2aed16d7f7957b44c595a65e3c45a71a
Jordy Rymenants <jordy.rymenants@outlook.com>
2019-02-18 09:53:35 +0100

    Moved host and port for mongoose connection string to secrets.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,7 +2,7 @@ const secrets = require('./secrets');
 const config = require('./config')
 
 const mongoose = require('mongoose');
-const db = mongoose.connect(`mongodb://${secrets.database.user}:${secrets.database.password}@193.191.177.167:27017/${secrets.database.db}`, {
+const db = mongoose.connect(`mongodb://${secrets.database.user}:${secrets.database.password}@${secrets.database.host}:${secrets.database.port}/${secrets.database.db}`, {
 	useNewUrlParser: true,
 	poolSize: config.database.poolSize
 });
