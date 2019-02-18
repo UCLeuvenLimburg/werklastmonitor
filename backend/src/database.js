971a2e6975f96ea18e982bafd1c3806be0699bae
Jordy Rymenants <jordy.rymenants@outlook.com>
2019-02-18 09:51:51 +0100

    Changed pool size for mongoose

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,10 @@
 const secrets = require('./secrets');
+const config = require('./config')
 
 const mongoose = require('mongoose');
-const db = mongoose.connect(`mongodb://${secrets.database.user}:${secrets.database.password}@193.191.177.167:27017/${secrets.database.db}`, { useNewUrlParser: true });
+const db = mongoose.connect(`mongodb://${secrets.database.user}:${secrets.database.password}@193.191.177.167:27017/${secrets.database.db}`, {
+	useNewUrlParser: true,
+	poolSize: config.database.poolSize
+});
 
 module.exports = db;
