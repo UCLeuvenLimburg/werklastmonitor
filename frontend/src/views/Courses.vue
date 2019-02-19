442a6d62d5153e17e44bb10c9c081441249b7b63
Tom L <lemmens.tom11@gmail.com>
2019-02-19 14:21:14 +0100

    Aanpassingen werkverhouding

--- frontend/src/views/Courses.vue
+++ frontend/src/views/Courses.vue
@@ -5,11 +5,15 @@
 			h2 {{ course }}
 			div(v-for="(lab, index) in assignments(course)" :key="lab.name")
 				h3 {{ lab.name }}
+				#progress
+					#bar(v-bind:style="getBarStyle(lab)")
+						p {{ getPercentage(lab) }}%
+				p {{ getWorkedHours(lab) }} uren gewerkt. De docent schat een gemiddelde van {{ lab.hourEstimate }} uren.
+				h4 Milestones
 				ul
 					li(v-for="(milestone, index) in lab.milestones" :key="milestone.name")
 						p.unchecked(v-on:click="check(milestone)", v-if="!milestone.isDone") {{ milestone.name }}
 						p.checked(v-on:click="uncheck(milestone)", v-if="milestone.isDone") {{ milestone.name }}
-
 </template>
 
 <script>
@@ -18,25 +22,42 @@ export default {
 	data () {
 		return {
 			labs: [{
+				id: 1,
 				name: 'Schrijfopdracht',
-				startDate: new Date(2019, 2, 2),
-				endDate: new Date(2019, 2, 3),
+				startDate: new Date(2019, 1, 1),
+				endDate: new Date(2019, 1, 12),
 				hourEstimate: 50,
 				course: { name: 'Computersystemen', fase: 1, courseCode: 'ABBA' },
-				milestones: [{ name: 'Eerste pagina', duration: 30, isDone: true }, { name: 'Tweede pagina', duration: 30, isDone: false }]
-			}, { name: 'Leesopdracht',
-				startDate: new Date(2019, 2, 10),
-				endDate: new Date(2019, 2, 10),
-				hourEstimate: 50,
+				milestones: [{ id: 1, name: 'Eerste pagina', duration: 30, isDone: true }, { id: 2, name: 'Tweede pagina', duration: 30, isDone: false }]
+			}, { id: 2,
+				name: 'Leesopdracht',
+				startDate: new Date(2019, 1, 10),
+				endDate: new Date(2019, 1, 10),
+				hourEstimate: 40,
 				course: { name: 'Computersystemen', fase: 1, courseCode: 'ABBA' },
-				milestones: [{ name: 'Gelezen', duration: 30, isDone: false }]
-			}, { name: 'Rudymoppen verzinnen',
-				startDate: new Date(2019, 3, 3),
-				endDate: new Date(2019, 3, 7),
+				milestones: [{ id: 1, name: 'Gelezen', duration: 30, isDone: false }]
+			}, { id: 3,
+				name: 'Rudymoppen verzinnen',
+				startDate: new Date(2019, 1, 3),
+				endDate: new Date(2019, 1, 7),
 				hourEstimate: 50,
 				course: { name: 'Netwerken', fase: 1, courseCode: 'RUDY' },
-				milestones: [{ name: 'Flauwe mop googlen', duration: 30, isDone: false }]
-			}]
+				milestones: [{ id: 1, name: 'Flauwe mop googlen', duration: 30, isDone: false }]
+			}],
+			worksessions: [{
+				id: 1,
+				startDate: new Date(2019, 1, 1),
+				endDate: new Date(2019, 1, 2),
+				lab: { id: 1, name: 'Schrijven' },
+				workdays: [{ day: new Date(2019, 1, 1), workhours: 5 }, { day: new Date(2019, 1, 2), workhours: 6 }]
+			}, {
+				id: 2,
+				startDate: new Date(2019, 1, 9),
+				endDate: new Date(2019, 1, 11),
+				lab: { id: 1, name: 'Schrijven' },
+				workdays: [{ day: new Date(2019, 1, 9), workhours: 5 }, { day: new Date(2019, 1, 10), workhours: 2 }, { day: new Date(2019, 1, 11), workhours: 5 }]
+			}],
+			bar: { width: '1%' }
 		};
 	},
 	computed: {
@@ -63,6 +84,33 @@ export default {
 		},
 		uncheck (m) {
 			m.isDone = false;
+		},
+		getPercentage (lab) {
+			let estimatedHours = lab.hourEstimate;
+			let workedHours = this.getWorkedHours(lab);
+			let percentage = workedHours / estimatedHours;
+			percentage *= 100;
+			return percentage;
+		},
+		getWorkedHours (lab) {
+			let workedHours = 0;
+			this.worksessions.forEach(worksession => {
+				if (worksession.lab.id === lab.id) {
+					worksession.workdays.forEach(workday => {
+						workedHours += workday.workhours;
+					});
+				}
+			});
+			return workedHours;
+		},
+		getBarStyle (lab) {
+			let style = {};
+			let percentage = this.getPercentage(lab);
+			if (percentage > 100) {
+				percentage = 100;
+			}
+			style.width = percentage + '%';
+			return style;
 		}
 	}
 };
@@ -74,5 +122,24 @@ export default {
 	li {
 		margin-left: 1rem;
 		cursor: pointer;
+	}
+	#progress {
+		width: 100%;
+		background-color: grey;
+	}
+
+	#bar {
+		height: 30px;
+		background-color: green;
+		color: white;
+		text-align: right;
+		font-weight: bold;
+		vertical-align: center;
+		line-height: 30px;
+	}
+	#bar p{
+		padding-right: 1em;
+		padding-left: 1em;
+		display: inline;
 	}
 </style>
