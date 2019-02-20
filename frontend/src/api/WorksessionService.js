2967ac6ac252b24749b1c22760e2bc9ff369124f
Tom L <lemmens.tom11@gmail.com>
2019-02-20 15:33:14 +0100

    Worksession

--- frontend/src/api/WorksessionService.js
+++ frontend/src/api/WorksessionService.js
@@ -2,18 +2,18 @@ import api from './api';
 
 export default {
 	post (worksession) {
-		return api.post('/worksessions', worksession);
+		return api().post('/worksessions', worksession);
 	},
 	put (worksession) {
-		return api.put('/worksessions/' + worksession._id, worksession);
+		return api().put('/worksessions/' + worksession._id, worksession);
 	},
 	delete (worksession) {
-		return api.delete('/worksessions/' + worksession._id);
+		return api().delete('/worksessions/' + worksession._id);
 	},
 	get () {
-		return api.get('/worksessions');
+		return api().get('/worksessions');
 	},
 	getId (id) {
-		return api.get('/worksessions/' + id);
+		return api().get('/worksessions/' + id);
 	}
 };
