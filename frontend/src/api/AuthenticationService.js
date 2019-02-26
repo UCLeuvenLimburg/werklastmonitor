0b6d711fb172296e942921f5dff127206e9fe802
Jordy Rymenants <jordy.rymenants@outlook.com>
2019-02-26 12:37:43 +0100

    Useless password field :D

--- frontend/src/api/AuthenticationService.js
+++ frontend/src/api/AuthenticationService.js
@@ -4,17 +4,10 @@ export default {
 	get () {
 		return api().get('/auth');
 	},
-	post (username) {
-		return api().post('/auth', {
-			username
-		});
-	}
-	/*
 	post (username, password) {
 		return api().post('/auth', {
 			username,
 			password
 		});
 	}
-	*/
 };
